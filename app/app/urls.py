3538faf8ef5c151a33e8d3d13c2a5f2dd880894b
BVLe <ble@crescendocollective.com>
2022-06-18 07:08:19 +0700

    Implemented user API.

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,6 +19,8 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
@@ -26,5 +28,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
 ]
